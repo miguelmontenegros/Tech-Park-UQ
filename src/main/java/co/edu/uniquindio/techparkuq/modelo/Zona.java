941227fa75445092f272870afb2e9825b5c359e8
miguelmontenegros <miguel.montenegros@uqvirtual.edu.co>
2026-05-09 19:23:30 -0500

    Fix: Corregidos imports y retornos en la clase Zona para asegurar compilación

--- src/main/java/co/edu/uniquindio/techparkuq/modelo/Zona.java
+++ src/main/java/co/edu/uniquindio/techparkuq/modelo/Zona.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.techparkuq.modelo;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Zona {
     private String nombre;
@@ -15,15 +16,24 @@ public class Zona {
     public void agregarAtraccion(Atraccion atraccion){
         listaAtracciones.add(atraccion);
     }
-    public String getNombre() {}
+    public String getNombre() {
+        return nombre;
+    }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    public String getDescripcion() {}
-    public void setDescripcion(String descripcion) {}
-    public List<Atracciones>getListaAtracciones(){return listaAtracciones;}
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public List<Atraccion>getListaAtracciones(){
+        return listaAtracciones;
+    }
+
     @Override
     public String toString(){
-        return "Zona{" + "nombre=" + nombre + "cantidad de Atracciones: "  + listaAtracciones+ "}" ;
+        return "Zona{" + "nombre=" + nombre + "cantidad de Atracciones: "  + listaAtracciones.size()+ "}" ;
     }
 }
