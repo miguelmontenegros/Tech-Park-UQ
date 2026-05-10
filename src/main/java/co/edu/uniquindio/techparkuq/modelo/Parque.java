550c3563370d1e5072c3bc1289083cb6e8c786a6
Sephiroth Leonardo Mota Arismendi <sephirothl.motaa@uqvirtual.edu.co>
2026-05-09 21:11:54 -0500

    registro de ingreso y documentacion

--- src/main/java/co/edu/uniquindio/techparkuq/modelo/Parque.java
+++ src/main/java/co/edu/uniquindio/techparkuq/modelo/Parque.java
@@ -68,6 +68,13 @@ public class Parque {
                 "Nómina de Empleados: " + listaEmpleados.size() + "\n" +
                 "Visitantes actuales: " + listaVisitantes.size();
     }
+    /**
+     * Valida si un visitante cumple los requisitos para entrar a una atracción.
+     * @param visitante El visitante que intenta ingresar.
+     * @param atraccion La atracción a la cual desea acceder.
+     * @return true si la atracción está activa y el visitante tiene la estatura mínima; false de lo contrario.
+     */
+
     public boolean validarAcceso(Visitante visitante, Atraccion atraccion){
         if(atraccion.getEstadoAtraccion().name().equals("MANTENIMIENTO")){
             System.out.println("ACCESO DENEGADO: "+ atraccion.getNombre() + " está en mantenimento.");
@@ -80,4 +87,18 @@ public class Parque {
         System.out.println("ACCESO CONCEDIDO: " + visitante.getNombre() + " ya puede disfrutar de " + atraccion.getNombre());
         return true;
     }
+    /**
+     * Registra el ingreso de un visitante al parque si supera la validación de seguridad.
+     * @param visitante El visitante que ingresa.
+     * @param atraccion La atracción a la que se dirige.
+     */
+    public void registrarIngreso(Visitante visitante, Atraccion atraccion) {
+        if (validarAcceso(visitante, atraccion)) {
+            listaVisitantes.add(visitante);
+            System.out.println("REGISTRO: " + visitante.getNombre() + " ha sido registrado en el sistema.");
+        } else {
+            System.out.println("REGISTRO FALLIDO: El visitante no cumple los requisitos de seguridad.");
+        }
+    }
+
 }
