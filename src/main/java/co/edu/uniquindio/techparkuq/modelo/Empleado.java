83aaadeab4aa9f7d0151e86de947f7e04949a610
miguelmontenegros <miguel.montenegros@uqvirtual.edu.co>
2026-05-14 18:42:22 -0500

    Refactor: Ajuste de clase Empleado

--- src/main/java/co/edu/uniquindio/techparkuq/modelo/Empleado.java
+++ src/main/java/co/edu/uniquindio/techparkuq/modelo/Empleado.java
@@ -4,10 +4,14 @@ import co.edu.uniquindio.techparkuq.modelo.abstractas.Persona;
 
 public class Empleado extends Persona {
     private String rol;
+    private int edad;
+    private double salario;
 
     public Empleado(String rol, String nombre, String documento, int edad) {
-        super(nombre, documento, edad);
+        super(nombre, documento);
         this.rol = rol;
+        this.edad = edad;
+        this.salario = 0.0;
     }
 
     public String getRol() {
@@ -18,8 +22,30 @@ public class Empleado extends Persona {
         this.rol = rol;
     }
 
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
     @Override
     public String toString() {
-        return "Nombre: " + getNombre() + "\n Documento: " + getDocumento() + "\n Edad: " + getEdad() + "\n Rol: " + rol;
+        return "Empleado {" +
+                "Nombre: '" + getNombre() + '\'' +
+                ", Documento: '" + getDocumento() + '\'' +
+                ", Rol: '" + rol + '\'' +
+                ", Edad: " + edad +
+                ", Salario: " + salario +
+                '}';
     }
 }
