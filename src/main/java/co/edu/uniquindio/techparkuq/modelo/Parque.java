df3c946f25198d80afb4d752eca7d863c31686ef
Sephiroth Leonardo Mota Arismendi <sephirothl.motaa@uqvirtual.edu.co>
2026-05-14 23:57:54 -0500

    Metodo cambiarEstadoClima y una propiedad con su cosntructor

--- src/main/java/co/edu/uniquindio/techparkuq/modelo/Parque.java
+++ src/main/java/co/edu/uniquindio/techparkuq/modelo/Parque.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.techparkuq.modelo;
 import co.edu.uniquindio.techparkuq.modelo.enums.EstadoAtraccion;
 import co.edu.uniquindio.techparkuq.modelo.interfaces.IGestionable;
+import co.edu.uniquindio.techparkuq.modelo.enums.TipoAlertaClimatica;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ public class Parque implements IGestionable {
     private List<Zona> listaZonas;
     private List<Empleado> listaEmpleados;
     private List<Visitante> visitantesActivos;
+    private TipoAlertaClimatica estadoClima;
 
     public Parque(String nombre, int capacidadMaxima) {
         this.nombre = nombre;
@@ -20,12 +22,16 @@ public class Parque implements IGestionable {
         this.listaZonas = new ArrayList<>();
         this.listaEmpleados = new ArrayList<>();
         this.visitantesActivos = new ArrayList<>();
-
+        this.estadoClima = TipoAlertaClimatica.NINGUNA;
     }
 
     public void contratarEmpleado(Empleado empleado) {
         listaEmpleados.add(empleado);
 
+    }
+
+    public TipoAlertaClimatica getEstadoClima() {
+        return estadoClima;
     }
 
     public void agregarZona(Zona zona) {
@@ -140,6 +146,16 @@ public class Parque implements IGestionable {
         return null;
     }
 
+    public void cambiarEstadoClima(TipoAlertaClimatica nuevaAlerta) {
+
+        this.estadoClima = nuevaAlerta;
+        System.out.println("\nSISTEMA CLIMA: Alerta cambiada a " + nuevaAlerta);
+        for (Zona zona : listaZonas) {
+            for (Atraccion atr : zona.getListaAtracciones()) {
+                atr.reaccionarAlClima(nuevaAlerta);
+            }
+        }
+    }
 
 
         @Override
