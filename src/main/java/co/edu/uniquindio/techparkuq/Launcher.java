b0667494150d20791f87c9a37c32050018f427da
miguelmontenegros <miguel.montenegros@uqvirtual.edu.co>
2026-05-21 20:11:00 -0500

    Corrección de dirección de paquete login

--- src/main/java/co/edu/uniquindio/techparkuq/Launcher.java
+++ src/main/java/co/edu/uniquindio/techparkuq/Launcher.java
@@ -10,7 +10,7 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/techparkuq/vista/VentanaPrincipal.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/techparkuq/vista/Login.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root);
